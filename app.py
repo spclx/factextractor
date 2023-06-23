552b64b09324526d69ca96235a37a0eb1fcc429e
spclx <48734004+spclx@users.noreply.github.com>
2023-06-23 14:08:32 +0300

    add tedt file

--- app.py
+++ app.py
@@ -8,9 +8,17 @@ st.title('Автоматический аннотатор')
 st.markdown("Скопируйте текст дневика в это поле или выберите для теста один из подготовленных отрывков.")
 
 diary = st.text_area('Текст дневника')
+if st.button('Быстрая обработка на тестовом тексте '):
+    with open('test.txt', 'r') as f:
+        diary = f.read()
+    df = d.analyze(diary)
+    st.dataframe(df)
+    for_chart = sp.data_for_sentiment_chart(df).set_index('n_date')
+    st.line_chart(data=for_chart)
 
 if st.button('Обработать'):
     df = d.analyze(diary)
     st.dataframe(df)
     for_chart = sp.data_for_sentiment_chart(df).set_index('n_date')
     st.line_chart(data=for_chart)
+
