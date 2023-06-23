bacf07cc65856983d27df9b1692988b428a25ed0
spclx <48734004+spclx@users.noreply.github.com>
2023-06-23 14:33:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ st.title('Автоматический аннотатор')
 
 st.markdown("Скопируйте текст дневика в это поле или выберите для теста один из подготовленных отрывков.")
 
-diary = st.text_area('Текст дневника')
+with open('test.txt', 'r') as f:
+    TEST = f.read()
+# diary = st.text_area('Текст дневника')
 if st.button('Быстрая обработка на тестовом тексте '):
-    with open('test.txt', 'r') as f:
-        diary = f.read()
-    df = d.analyze(diary)
+    df = d.analyze(TEST)
     st.dataframe(df)
     for_chart = sp.data_for_sentiment_chart(df).set_index('n_date')
     st.markdown('### График сентимента по записям дневника (тест)')
     st.line_chart(data=for_chart)
 
-if st.button('Обработать'):
-    df = d.analyze(diary)
-    st.dataframe(df)
-    for_chart = sp.data_for_sentiment_chart(df).set_index('n_date')
-    st.markdown('### График сентимента по записям дневника (тест)')
-    st.line_chart(data=for_chart)
+# if st.button('Обработать'):
+#     df = d.analyze(diary)
+#     st.dataframe(df)
+#     for_chart = sp.data_for_sentiment_chart(df).set_index('n_date')
+#     st.markdown('### График сентимента по записям дневника (тест)')
+#     st.line_chart(data=for_chart)
 
